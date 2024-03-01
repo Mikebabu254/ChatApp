07f2007339f4e5e532cc84ca296a30470cbd41e9
Michael ndolo <michaelndolo17142@gmail.com>
2024-03-01 13:42:32 +0300

    functionality

--- app/src/main/java/com/example/chatapp/LoginPhoneNumber.java
+++ app/src/main/java/com/example/chatapp/LoginPhoneNumber.java
@@ -3,12 +3,31 @@ package com.example.chatapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+
+import com.hbb20.CountryCodePicker;
 
 public class LoginPhoneNumber extends AppCompatActivity {
+    CountryCodePicker countryCodePicker;
+    EditText phoneNo;
+    Button button;
+    ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_phone_number);
+
+        countryCodePicker = findViewById(R.id.county_code);
+        phoneNo = findViewById(R.id.mobile_number);
+        button = findViewById(R.id.send_opt_btn);
+        progressBar = findViewById(R.id.mobile_number_loader);
+
+        progressBar.setVisibility(View.GONE);
+
+
     }
 }
